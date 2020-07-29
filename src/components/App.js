077a23fc32096a40088b947752a7e4518eb82558
Felipe <jufelipe.la@gmail.com>
2020-07-28 20:30:12 -0700

    login working

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import "antd/dist/antd.css";
+import { auth } from "./../_actions/user/user_actions";
 
 import LoginPage from "./views/LoginPage/LoginPage";
 import Auth from "../hoc/auth";
@@ -12,17 +13,19 @@ import AddHairCut from "./views/Forms/AddHaircut";
 import { useSelector, useDispatch } from "react-redux";
 
 const ProtectedRoute = ({ component: Component, user, ...rest }) => {
+  console.log('here', user)
   return (
     <Route
       {...rest}
       render={(props) => {
-        if (user.logged) { // or user.id, user.isLogged, token, etc anything that indicates the user is actually logged in 
+        if (user.userData && user.userData.loginSuccess) {
+          // or user.id, user.isLogged, token, etc anything that indicates the user is actually logged in
           return <Component {...rest} {...props} />;
         } else {
           return (
             <Redirect
               to={{
-                pathname: "/register", // or any other 
+                pathname: "/register", // or any other
                 state: {
                   from: props.location,
                 },
@@ -35,14 +38,19 @@ const ProtectedRoute = ({ component: Component, user, ...rest }) => {
   );
 };
 function App() {
-  let user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
+  console.log("user ===> ", user);
+  useEffect(() => {
+    dispatch(auth());
+  }, []);
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <NavBar />
       <div style={{ paddingTop: "69px", minHeight: "calc(100vh - 80px)" }}>
         <Switch>
-          <Route exact path="/" component={Auth(LoginPage, null)} />
-          <Route exact path="/register" component={Auth(RegisterPage, null)} />
+          <Route exact path="/" component={LoginPage} />
+          <Route exact path="/register" component={RegisterPage} />
           <ProtectedRoute exact path="/home" component={HomePage} user={user} />
           <ProtectedRoute
             exact
